8e632b1382d1a4d5d3f3853ae70ac7a5407c8a00
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2025-02-24 16:21:28 -0600

    routing for home and cart page

---
+++ my-app/src/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div>
+      <h1>Cart Page</h1>
+      <p>Your shopping cart is empty.</p>
+    </div>
+  );
+}
+
+export default Cart;
