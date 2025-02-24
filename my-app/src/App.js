52fa8da74f364020468953343e996e1ceee420f2
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2025-02-24 16:40:11 -0600

    sidebar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Cart from "./Cart";
-import Header from "./Header"; 
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import "./Home.css"; 
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to our store!</p>
+    <div className="home-container">
+      <Header /> 
+      <div className="content-container">
+        <Sidebar /> 
+        <div className="main-content">
+          <h1>Welcome to DroneDash</h1>
+          <p>Browse categories and find your favorite meals.</p>
+        </div>
+      </div>
     </div>
   );
 }
