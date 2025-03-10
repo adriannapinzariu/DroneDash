3fab836c1b51212296585990349f2c9fcf35afb0
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2025-03-10 14:32:28 -0500

    imported header and sidebar

--- my-app/src/RestaurantPage.js
+++ my-app/src/RestaurantPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { restaurantData } from "./data/restaurants";
 import "./RestaurantPage.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function RestaurantPage() {
   const { id } = useParams();
@@ -12,37 +14,48 @@ function RestaurantPage() {
   }
 
   return (
-    <div className="restaurant-page">
-      <div className="restaurant-header">
-        <img src={restaurant.image} alt={restaurant.name} className="restaurant-img" />
-        <div className="restaurant-logo">
-          <img src={restaurant.logo} alt={restaurant.name} />
-        </div>
-      </div>
+    <div className="restaurant-layout">
+      {/* Sidebar */}
+      <Sidebar />
 
-      <div className="restaurant-info">
-        <h1>{restaurant.name}</h1>
-        <p>⭐ {restaurant.rating} ({restaurant.reviews} ratings) • {restaurant.cuisine} • {restaurant.distance}</p>
-        <div className="tags">
-          {restaurant.friendlyTags.map((tag, index) => (
-            <span key={index} className="tag">{tag}</span>
-          ))}
-        </div>
+      <div className="main-content">
+        {/* Header */}
+        <Header />
 
-        <div className="delivery-details">
-          <button className="delivery-option">Delivery</button>
-          <button className="pickup-option">Pickup</button>
-          <button className="group-order">Group Order</button>
-          <div className="delivery-fee">
-            {restaurant.fee} <span>{restaurant.deliveryTime} delivery time</span>
+        {/* Restaurant Page Content */}
+        <div className="restaurant-page">
+          <div className="restaurant-header">
+            <img src={restaurant.image} alt={restaurant.name} className="restaurant-img" />
+            <div className="restaurant-logo">
+              <img src={restaurant.logo} alt={restaurant.name} />
+            </div>
+          </div>
+
+          <div className="restaurant-info">
+            <h1>{restaurant.name}</h1>
+            <p>⭐ {restaurant.rating} ({restaurant.reviews} ratings) • {restaurant.cuisine} • {restaurant.distance}</p>
+            <div className="tags">
+              {restaurant.friendlyTags.map((tag, index) => (
+                <span key={index} className="tag">{tag}</span>
+              ))}
+            </div>
+
+            <div className="delivery-details">
+              <button className="delivery-option">Delivery</button>
+              <button className="pickup-option">Pickup</button>
+              <button className="group-order">Group Order</button>
+              <div className="delivery-fee">
+                {restaurant.fee} <span>{restaurant.deliveryTime} delivery time</span>
+              </div>
+            </div>
+
+            {restaurant.deals && (
+              <div className="deals">
+                <p>🔥 {restaurant.deals}</p>
+              </div>
+            )}
           </div>
         </div>
-
-        {restaurant.deals && (
-          <div className="deals">
-            <p>🔥 {restaurant.deals}</p>
-          </div>
-        )}
       </div>
     </div>
   );
